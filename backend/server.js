19393814661f71ba5db027d3b2133409ae7af1af
rupesh8690 <rupeshlove8690@gmail.com>
2024-12-23 18:16:42 +0545

    deployment

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js"
@@ -14,6 +15,7 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 
 
 const PORT= process.env.PORT || 5000; //port value should be either 5000 or value from env file
+const __dirname=path.resolve();
 
 dotenv.config();
 app.use(express.json());//to parse the incoming requests with JSON payloads (from req.body)
@@ -23,6 +25,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes)
 
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+//if any router other than above
+app.get("*",(req,res) =>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"));
+
+})
 // app.get("/",(req,res) =>{
 //     //root route http://localhost:5000/
 //     res.send("welcome to the world of programming");
